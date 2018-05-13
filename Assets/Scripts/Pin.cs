3d1b487bfdacffbcab7289715d1e0699e472da22
danijel <danijel.husakovic@gmail.com>
2018-05-13 18:23:53 +0200

    Each pin now detects whether it's standing within 3 degrees eulerrotation or not. PinSetter script now checks accurately how many pins are still standing, and prints it to the text element of UI. 'bug' might be that the text changes because some pins, whilst flying through the air, briefly reach the 3 degree threshold.

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+
+	public float standingThreshold;
+
+	// Use this for initialization
+	void Start () {
+		standingThreshold = 3f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public bool IsStanding(){
+		Vector3 rotationInEuler = transform.rotation.eulerAngles;
+
+		float tiltInX = Mathf.Abs(rotationInEuler.x);
+		// Y is rotation around the central axis of the pin. which is irrelevant. it still stands.
+		float tiltInZ = Mathf.Abs(rotationInEuler.z);
+		// abs makes it so that -10 degrees = 350 degrees.
+
+		if(tiltInX  < standingThreshold && tiltInZ < standingThreshold){
+			return true;
+		}
+
+		return false;
+
+	}
+
+}
