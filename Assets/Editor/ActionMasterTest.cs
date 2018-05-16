3a2dad1b03a60cd907abdc86b7de694adc772532
danijel <danijel.husakovic@gmail.com>
2018-05-16 23:51:02 +0200

    Continued writing tests. For detecting end of frame and bowling 2 and 8 etc. . .

--- Assets/Editor/ActionMasterTest.cs
+++ Assets/Editor/ActionMasterTest.cs
@@ -7,6 +7,13 @@ using UnityEngine;
 public class ActionMasterTest {
 
 	private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+	private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
+	private ActionMaster actionMaster;
+
+	[SetUp]
+	public void Setup(){ // happens every time the test runs
+		actionMaster = new ActionMaster();
+	}
 
 	[Test]
 	public void T00PassingTest(){
@@ -15,7 +22,18 @@ public class ActionMasterTest {
 
 	[Test]
 	public void T01OneStrikeReturnsEndTurn(){
-		ActionMaster actionMaster = new ActionMaster();
 		Assert.AreEqual (endTurn, actionMaster.Bowl(10));
 	}
+
+	[Test]
+	public void T02Bowl8ReturnsTidy(){
+		Assert.AreEqual (tidy, actionMaster.Bowl(8));
+	}
+
+	[Test]
+	public void T03Bowl28SpareReturnsEndTurn(){
+		actionMaster.Bowl(8);
+		Assert.AreEqual (endTurn, actionMaster.Bowl(2));
+	}
+
 }
--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -6,6 +6,9 @@ public class ActionMaster {
 
 	public enum Action { Tidy, Reset, EndTurn, EndGame };
 
+	// private int[] bowls = new int[21];
+	private int bowl = 1;
+
 	public Action Bowl (int pins){
 
 		if(pins < 0 || pins > 10){ throw new UnityException ("Invalid number of pins"); }
@@ -13,10 +16,18 @@ public class ActionMaster {
 		// Other behavior here
 
 		if(pins == 10){
+			bowl += 2;
 			return Action.EndTurn;
 		}
 
-		// Other behavior here
+		if (bowl % 2 != 0){ // Mid frame or last frame (it's not end of frame)
+			bowl += 1;
+			return Action.Tidy;
+		} else if (bowl % 2 == 0){ // End of frame
+			bowl += 1;
+			return Action.EndTurn;
+		}
+
 
 		throw new UnityException ("Not sure what action to return!"); // hope it never reaches this line :)
 
