4f67621563938bde8f344b57e82ae964c245328a
danijel <danijel.husakovic@gmail.com>
2018-05-17 17:54:32 +0200

    Finished the control code. All tests pass.

--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -6,14 +6,25 @@ public class ActionMaster {
 
 	public enum Action { Tidy, Reset, EndTurn, EndGame };
 
-	// private int[] bowls = new int[21];
+	private int[] bowls = new int[21];
 	private int bowl = 1;
 
 	public Action Bowl (int pins){
-
 		if(pins < 0 || pins > 10){ throw new UnityException ("Invalid number of pins"); }
 
-		// Other behavior here
+		bowls [bowl - 1] = pins;
+
+		if(bowl == 21){ 
+			return Action.EndGame;
+		}
+
+		// Handle last-frame special cases
+		if(bowl >= 19 && Bowl21Awarded()){
+			bowl += 1;
+			return Action.Reset;
+		} else if(bowl == 20 && ! Bowl21Awarded()){
+			return Action.EndGame;
+		}
 
 		if(pins == 10){
 			bowl += 2;
@@ -31,6 +42,11 @@ public class ActionMaster {
 
 		throw new UnityException ("Not sure what action to return!"); // hope it never reaches this line :)
 
+	}
+
+	private bool Bowl21Awarded(){
+		// -1 are because arrays start at 0, and he wanted to write human language :)
+		return (bowls [19 - 1] + bowls[20 - 1] >= 10);
 	}
 
 }
