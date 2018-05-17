ab4d77b036e847e1b3095b96239be6ae4ce0fbdd
danijel <danijel.husakovic@gmail.com>
2018-05-17 19:00:05 +0200

    Added more test from the community and made them all pass. Concluded there will always be a possibility of a new VALID test that is red, so the best approach would be to find the rules for bowling, and turn them all into tests. But we're moving on.

--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -19,22 +19,33 @@ public class ActionMaster {
 		}
 
 		// Handle last-frame special cases
-		if(bowl >= 19 && Bowl21Awarded()){
-			bowl += 1;
+		if(bowl >= 19 && pins == 10){
+			bowl ++;
 			return Action.Reset;
-		} else if(bowl == 20 && ! Bowl21Awarded()){
-			return Action.EndGame;
+		} else if(bowl == 20){
+			bowl++;
+			if(bowls[19 - 1] == 10 && bowls[20 - 1] == 0){
+				return Action.Tidy;
+			}
+			else if ((bowls[19 - 1] + bowls[20 - 1]) % 10 == 0 ) {
+				return Action.Reset;
+			} else if (Bowl21Awarded ()) {
+				return Action.Tidy;
+			} else {
+				return Action.EndGame;	
+			}
 		}
 
-		if(pins == 10){
-			bowl += 2;
-			return Action.EndTurn;
-		}
 
-		if (bowl % 2 != 0){ // Mid frame or last frame (it's not end of frame)
-			bowl += 1;
-			return Action.Tidy;
-		} else if (bowl % 2 == 0){ // End of frame
+		if (bowl % 2 != 0){ // First bowl of frame
+			if (pins == 10) {
+				bowl += 2;
+				return Action.EndTurn;
+			} else {
+				bowl += 1;
+				return Action.Tidy;
+			}
+		} else if (bowl % 2 == 0){ // Second bowl of frame
 			bowl += 1;
 			return Action.EndTurn;
 		}
