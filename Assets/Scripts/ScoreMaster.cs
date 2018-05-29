3a5d506e39b1f1fbffb7d672a6b5b33890647653
danijel <danijel.husakovic@gmail.com>
2018-05-29 23:53:17 +0200

    Downloaded the tests for scoremaster, and started writing the code that gradually passes the 20+ tests. Now at test #9.

--- Assets/Scripts/ScoreMaster.cs
+++ Assets/Scripts/ScoreMaster.cs
@@ -4,9 +4,38 @@ using UnityEngine;
 
 public class ScoreMaster {
 
-	public static List<int> ScoreFrames (List<int> rolls) {
-		List <int> frameList = new List <int> ();
+	// Returns a list of cumulative scores, like a normal score card.
+	public static List<int> ScoreCumulative (List<int> rolls){
+		List<int> cumulativeScores = new List<int> ();
+		int runningTotal = 0;
 
-		return frameList;
+		foreach(int frameScore in ScoreFrames (rolls)){
+			runningTotal += frameScore;
+			cumulativeScores.Add(runningTotal);
+		}
+
+		return cumulativeScores;
+	}
+
+	// Returns a list of individual frame scores, NOT cumulative.
+	public static List<int> ScoreFrames (List<int> rolls) {
+		List <int> frames = new List <int> ();
+
+		for (int i = 1; i < rolls.Count; i+= 2){
+
+			if(rolls[i - 1] + rolls[i] < 10){	// Normal "open" frame
+				frames.Add (rolls[i - 1] + rolls[i]);
+			}
+
+			if(rolls.Count - i <= 1) {			// Insufficient look-ahead
+				break;
+			}
+
+			if(rolls[i - 1] + rolls[i] == 10){	// Calculate Spare bonus
+				frames.Add (10 + rolls[i + 1]);
+			}
+		}
+
+		return frames;
 	}
 }
